cec8728a7b2244f02107fd4afae8b11364dd11d2
Henry <henrypye@outlook.com>
2018-02-06 17:53:49 +0000

    Started to create sessions

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var session = require('express-session');
 var app = express();
 
 /*  Templates
@@ -6,12 +7,25 @@ var app = express();
 app.use(express.static('views'));
 app.use(express.static('views'));
 
-var indextemplate = require('pug').compileFile(__dirname + '/views/index.pug');
+var indextemplate = require('pug').compileFile(__dirname + '/views/index.pug'),
+    logintemplate = require('pug').compileFile(__dirname + '/views/login.pug');
 
+/*  Session Handling
+*/
+
+app.use(session({
+    secret: 'wotcha',
+    resave: false,
+
+}));
 var port = 8080;
 
 app.get('/', function(req, res, next) {
     res.send(indextemplate({ title: 'Homepage' }));
+});
+
+app.get('/login', function(req, res, next) {
+    res.send(logintemplate({ title: 'Login' }));
 });
 
 app.listen(port, function() {
