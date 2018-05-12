78420c02771126870638bd01bc07383572756c2c
Henry <henrypye@outlook.com>
2018-05-12 12:40:00 +0100

    Header now works...

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Header } from './components';
 
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className = "App-header">
-          <Header />
-        </div>
-      </div>
-    );
-  }
+export default () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
 }
-
-export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Header, Icon } from 'semantic-ui-react';
 
+import './Header.css';
+
 export default () => {
     return (
-        <Header 
-            as='h2' 
-            icon 
-            textAlign='center' 
-            inverted 
-            color = 'white'>
-            <Icon name='compass' circular />
-            <Header.Content>
-                Tracker
-            </Header.Content>
-        </Header>
+        <div className = "App-header">         
+            <Header 
+                as='h2' 
+                icon 
+                textAlign='center' 
+                inverted 
+                color = 'white'>
+                <Icon name='compass' circular />
+                <Header.Content>
+                    Tracker
+                </Header.Content>
+            </Header>
+        </div>
     );
 }
