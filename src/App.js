2ebfd0129e0c6da6d8ac416a9c4c975205db6ced
Henry Pye <henrypye@outlook.com>
2018-05-13 16:37:13 +0100

    Fixed tests (again) App.test.js not working...

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import { Header, Overview } from './components';
 import './App.css';
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
-
-const middlewares = [thunk] // add your middlewares like `redux-thunk`
-const mockStore = configureStore(middlewares)
 
 export default () => {
-  const store = mockStore({
-    devices : {
-        devices : []
-    }
-  })
-
   return (
     <div className="App">
-      <Provider store = {store}>
-        <Header />
-        <Overview />
-      </Provider>
+      <Header />
+      <Overview />
     </div>
   )
 }
