1eb7f215da5c358c579fdc584655730aa0dfff42
Henry Pye <henrypye@outlook.com>
2018-05-13 16:29:45 +0100

    Working tests

--- src/components/Accounts/Feed/FeedTest.test.js
+++ src/components/Accounts/Feed/FeedTest.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Feed from './Feed';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+
+const middlewares = [thunk] // add your middlewares like `redux-thunk`
+const mockStore = configureStore(middlewares)
 
 test('the feed renders', () => {
+    const store = mockStore({
+        devices : {
+            devices : []
+        }
+    })
     var div = document.createElement('div');
-    ReactDOM.render(<Feed />, div);
+    ReactDOM.render(
+    <Provider store = {store}>
+        <Feed />
+    </Provider>, div);
     ReactDOM.unmountComponentAtNode(div);
 })
