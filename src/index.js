bd25d132cdb203b2c1a76379fe10aa2d7776251a
Henry Pye <henrypye@outlook.com>
2018-05-13 12:37:02 +0100

    Very basic Devices imported

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import registerServiceWorker from './registerServiceWorker';
+
+/* App Component */
+import App from './App';
+
+/* CSS */
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 
-import App from './App';
+/* Redux */
+import { Provider } from 'react-redux';
 
-import registerServiceWorker from './registerServiceWorker';
+import * as stateActions from './store/stateManager';
+import * as deviceActions from './containers/Devices/action';
 
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+store.dispatch(deviceActions.getDevices());
+
+store.subscribe(() => {
+    stateActions.saveState(store.getState());
+});
+
+ReactDOM.render(
+            <Provider store = {store}>
+                <App />
+            </Provider>, 
+            document.getElementById('root')
+        );
 registerServiceWorker();
