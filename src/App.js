944d3866c730f608c5bc0fc2b38f5f89241b4051
Henry Pye <henrypye@outlook.com>
2018-05-13 16:35:01 +0100

    Fixed tests

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { Header, Overview } from './components';
 import './App.css';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+
+const middlewares = [thunk] // add your middlewares like `redux-thunk`
+const mockStore = configureStore(middlewares)
 
 export default () => {
+  const store = mockStore({
+    devices : {
+        devices : []
+    }
+  })
+
   return (
     <div className="App">
-      <Header />
-      <Overview />
+      <Provider store = {store}>
+        <Header />
+        <Overview />
+      </Provider>
     </div>
   )
 }
