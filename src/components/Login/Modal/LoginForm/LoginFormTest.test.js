9090ea3ec8f5c1727e05b027b86b1594c31193fb
Henry Pye <henrypye@outlook.com>
2018-05-14 10:58:26 +0100

    Fixed LoginForm test by mocking store

--- src/components/Login/Modal/LoginForm/LoginFormTest.test.js
+++ src/components/Login/Modal/LoginForm/LoginFormTest.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginForm from './LoginForm';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares)
 
 it('renders the form without crashing', () => {
+    const store = mockStore({
+        users : {
+            token : null
+        },
+        devices : {
+            devices : []
+        }
+    });
+
     var div = document.createElement('div');
-    ReactDOM.render(<LoginForm />, div);
+    ReactDOM.render(
+        <Provider store = {store}>
+            <LoginForm />
+        </Provider>, div);
     ReactDOM.unmountComponentAtNode(div);
 })
