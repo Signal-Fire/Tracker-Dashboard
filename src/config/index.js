0401c564aeabd54b630022b6a723668647e8634a
Henry Pye <henrypye@outlook.com>
2018-05-15 10:06:47 +0100

    Redux integration now working

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export const USER_COOKIE_IDENTIFIER = "tracker.app:store";
-export const API_URL = "http://localhost:8080/api";
+export const API_URL = "http://tracker-api.ukwest.cloudapp.azure.com:8080/api";
 
 export const DELETE_DEVICE_URL = API_URL + "/delete";
 export const FIND_ALL_DEVICES_URL = API_URL + "/find/devices/all";
