1cdfe1106c89374cb56d9f8e2be38803e702c2d9
Henry Pye <henrypye@outlook.com>
2018-05-16 09:56:49 +0100

    Slight test coverage improvements

--- src/containers/Devices/reducerTest.test.js
+++ src/containers/Devices/reducerTest.test.js
@@ -36,5 +36,39 @@ describe('actions', () => {
         return store.dispatch(actions.getDevices()).then(() => {
             expect(store.getActions()).toEqual(expected)
         })
+    }),
+    it ('should receive the new device being added', () =>{
+        var store = mockStore({
+            devices : [
+                {
+                    name : 'Ratchetts Plasma',
+                    type : 'Gun'
+                }
+            ]
+        })
+
+        var newDevice = {
+            name : 'Thors Hammer',
+            type : 'Hammer'
+        }
+
+        var expected = [{
+            payload : {
+                error : false,
+                newDevice : {
+                    name : 'Thors Hammer',
+                    type : 'Hammer'
+                }
+            },
+            type : actionTypes.ADD_DEVICE
+        }]
+
+        Mock.onPost(config.CREATE_DEVICE_URL).reply(200, newDevice);
+
+        return store.dispatch(actions.addDevice("phone", "phone", () => {
+            console.log('hey')
+        })).then(() => {
+            expect(store.getActions()).toEqual(expected);
+        })
     })
 })
